8d2b503b86bb27ee7a85f46d04b15a92ac856f84
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-11-03 21:13:02 -0300

    feat: add associar endereco ao usuario

--- src/controllers/enderecoController.ts
+++ src/controllers/enderecoController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express'
-import { Op } from 'sequelize'
 import { Endereco } from '../models/enderecoModel'
 import { jwtDecode } from 'jwt-decode'
 import { JwtPayload } from 'jsonwebtoken'
