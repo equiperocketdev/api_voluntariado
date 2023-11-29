0de2cefb6d0885c1c53a538372a38e4ad80beda9
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-11-29 20:14:40 -0300

    update: url database

--- src/db/pg.ts
+++ src/db/pg.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 //Config variaveis de ambiente
 dotenv.config();
 
-//export const sequelize = new Sequelize(process.env.DATABASE_URL as string)
+export const sequelize = new Sequelize(process.env.DATABASE_URL as string)
 
-export const sequelize = new Sequelize(
+/* export const sequelize = new Sequelize(
     process.env.PG_DB as string,
     process.env.PG_USER as string,
     process.env.PG_PASSWORD as string,
@@ -14,4 +14,4 @@ export const sequelize = new Sequelize(
         dialect: 'postgres',
         port: parseInt(process.env.PG_DB_PORT as string)
     }
-);
+); */
